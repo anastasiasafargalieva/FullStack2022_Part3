d6246c668fdd2d9b52934d2eba7e5ad9ab47b77c
anastasiasafargalieva <94184868+anastasiasafargalieva@users.noreply.github.com>
2022-02-23 23:32:08 +0200

    3.2

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -33,3 +33,9 @@ const persons = [
 app.get("/api/persons", (req, res)=>{
     res.json(persons)
 }) 
+
+app.get("/info", (req, res)=>{
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+    )
+}) 
