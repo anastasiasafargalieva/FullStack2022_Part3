b6788cd45622f53b700fce98076bdba4508eefb9
anastasiasafargalieva <94184868+anastasiasafargalieva@users.noreply.github.com>
2022-02-24 00:30:56 +0200

    3.8*: Phonebook backend step8

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -11,7 +11,8 @@ app.listen(PORT, () => {
 })
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('reqbody', req => JSON.stringify(req.body) )
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
 
 let persons = [
     {
