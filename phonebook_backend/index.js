c79eb54c48883a213f35518804fcbacd8e6726b0
anastasiasafargalieva <94184868+anastasiasafargalieva@users.noreply.github.com>
2022-02-24 11:35:04 +0200

    heroku deployment

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
-app.use(express.json())
+app.use(cors()) 
+app.use(express.static('build')) 
+app.use(express.json()) 
 morgan.token('reqbody', req => JSON.stringify(req.body) )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqbody'))
 
