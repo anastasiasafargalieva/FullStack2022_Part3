db075edaf09d44f1ec31f30ef8cd03a440dd01d3
anastasiasafargalieva <94184868+anastasiasafargalieva@users.noreply.github.com>
2022-02-26 10:02:31 +0200

    3.20

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,89 +1,92 @@
-require("dotenv").config()
+require('dotenv').config()
 const express = require('express')
 const app = express()
-var morgan = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
-
-
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
 const Person = require('./src/models/person')
 
+
+// setup morgan
 morgan.token('reqbody', req => JSON.stringify(req.body) ) // morgan token
 const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :reqbody')
 
-app.use(cors()) 
-app.use(express.static('build')) 
-app.use(express.json()) 
-app.use(requestLogger)
+// middlewares
+app.use(cors()) // enable cors
+app.use(express.static('build')) // serve static contents from build folder
+app.use( express.json() ) // transform every json request to json
+app.use( requestLogger ) // print all requests
 
-
-app.get("/api/persons", (req, res)=>{
-    Person.find({}).then( result => res.json(result) )
-}) 
-
-app.get("/info", (req, res)=>{
-    Person.count({}, function( err, count){
-        res.json(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
-    })
-}) 
-
-app.get("/api/persons/:id", (req, res, next)=>{
-    const id = req.params.id
-    Person.findById(id).then( person => {
-        if (person) res.json(person)
-        else res.status(404).json({error: `Person with id ${id} not found.`})
-    }).catch( error => next(error))
+// port binding
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
 
-app.delete("/api/persons/:id", (req, res, next)=> {
-    Person.findByIdAndRemove( req.params.id ).then( result => {
-        res.status(204).end()
-    }).catch( error => next(error) )
+
+// endpoints
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then( result => response.json(result) )
 })
 
-app.post("/api/persons", (req, res, next)=>{
-    const newPerson = req.body
-    if (newPerson === undefined) res.status(400).json( {error:"Content missing"} )
-    if (!newPerson.name) return res.status(400).json({error:"Missing name property"})
-    if (!newPerson.number) return res.status(400).json({error:"Missing number property"})
-    
-
-    const person = new Person( {
-        name: newPerson.name,
-        number: newPerson.number
-    } )
-    person.save()
-        .then(savedPerson => res.json(savedPerson))
-        .catch( error => next(error) )
+app.get('/info', (request, response) => {
+  Person.count({}, function( err, count){
+    response.json(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
+  })
 })
 
-app.put("/api/persons/:id", (req, res, next) => {
-    const updateRequest = {
-        name: req.body.name,
-        number: req.body.number
-    }
-    Person.findByIdAndUpdate( req.params.id, updateRequest, {new: true} )
-        .then( updatedPerson => res.json(updatedPerson) )
-        .catch( error => next(error) )
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id).then( person => {
+    if (person) response.json(person)
+    else response.status(404).json({ error: `Person with id ${id} not found.` })
+  }).catch( error => next(error))
 })
 
-const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove( request.params.id ).then( () => {
+    response.status(204).end()
+  }).catch( error => next(error) )
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const newEntry = request.body
+  if (newEntry === undefined) response.status(400).json( { error:'Content missing' } )
+  if (!newEntry.name) return response.status(400).json({ error:'Missing name property' })
+  if (!newEntry.number) return response.status(400).json({ error:'Missing number property' })
+
+  const person = new Person( {
+    name: newEntry.name,
+    number: newEntry.number
+  } )
+  person.save()
+    .then( savedPerson => response.json(savedPerson) )
+    .catch( error => next(error) )
+})
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const updateRequest = {
+    name: request.body.name,
+    number: request.body.number
+  }
+  Person.findOneAndUpdate( request.params.id, updateRequest, { new: true } )
+    .then( updatedPerson => response.json(updatedPerson) )
+    .catch( error => next(error) )
+})
+
+
+// more event handlers
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
-const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError') return res.status(400).send({ error: 'malformatted id' })
-    else if (error.name === 'ValidationError') return res.status(400).json({ error: error.message })
-    next(error)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' })
+  else if (error.name === 'ValidationError') return response.status(400).json({ error: error.message })
+  next(error)
 }
-app.use( errorHandler )
-
-//to deploy heroku use command  $ git subtree push --prefix phonebook_backend heroku main
+app.use( errorHandler ) // handle errors
